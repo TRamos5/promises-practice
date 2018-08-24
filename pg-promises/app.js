efb084e89a58fd8f4dd781ac118d684c0fb3d85e
TRamos5 <travis.ramos5@gmail.com>
2018-08-24 15:54:21 -0500

    finished

--- pg-promises/app.js
+++ pg-promises/app.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let app = express();
+var prompt = require('prompt-promise');
+
 
 
 // Bluebird is the best promise library available today,
@@ -18,7 +20,7 @@ const initOptions = {
 const config = {
     host: 'localhost',
     port: 5432,
-    database: 'veronica2',
+    database: 'music_db',
     user: 'postgres'
 };
 
@@ -30,7 +32,82 @@ const pgp = require('pg-promise')(initOptions);
 const db = pgp(config);
 
 
+var res = [];
 
+// prompt('Album Name? ')
+// .then(function name(val) {
+//   res.push(val);
+//   return prompt('Album Year? ');
+// })
+// .then(function year(val) {
+//   res.push(val);
+//   return prompt('Artist ID? ');
+// })
+// .then(function id(val){
+//     res.push(val);
+//     console.log(res);
+//     prompt.done();
+//     var query = (`insert into album\
+//     values (default, '${res[0]}', '${res[1]}', ${res[2]})`);
+//     db.result(query)
+//         .then(function(result){
+//         console.log(result);
+//         });
+// })
+// .catch(function rejected(err) {
+//   console.log('error:', err.stack);
+//   prompt.finish();
+// });
+
+// var art = [];
+// prompt('Artist name? ')
+// .then(function artistname(val) {
+//     art.push(val);
+//     prompt.done();
+//     var query = (`insert into artist\
+//     values (default, '${art[0]}')`);
+//     db.result(query)
+//         .then(function(result){
+//         console.log(result);
+//         });
+// })
+// .catch(function rejected(err) {
+//     console.log('error:', err.stack);
+//     prompt.finish();
+// });
+
+var track = [];
+prompt('Track name?')
+.then(function trackname(val){
+    track.push(val);
+    return prompt('Track duration? ');
+})
+.then(function trackduration(val){
+    track.push(val);
+    return prompt('Album ID? ');
+})
+.then(function albumid(val){
+    track.push(val);
+    console.log(track);
+    prompt.done();
+    var query = (`insert into track\
+    values (default, '${track[0]}', '${track[1]}', ${track[2]})`);
+    db.result(query)
+        .then(function(result){
+        console.log(result);
+        });
+})
+.catch(function rejected(err){
+    console.log('error: ', err.stack);
+    prompt.finish();
+});
+
+// db.query('select * from music_db')
+//     .then(function(result){
+//         results.forEach(function (r){
+//             console.log(r.id, )
+//         })
+//     })
 
 
 
@@ -42,3 +119,7 @@ var server = app.listen(4000);
 server.on('close', function(){
     pgp.end();
 });
+
+
+
+//make it print "create tradck with id: """"
---
+++ pg-promises/template.sql
@@ -0,0 +1,21 @@
+
+
+-- create table album(
+--     id serial not null primary key,
+--     name varchar, 
+--     year integer,
+--     artist_id integer references artist (id)
+-- );
+
+-- create table artist(
+--     id serial not null primary key,
+--     name varchar
+-- );
+
+-- create table track(
+--     id serial not null primary key,
+--     name varchar,
+--     duration time,
+--     album_id integer references album (id)
+-- );
+
